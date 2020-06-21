a3a4710ff2a8cf213f3f80b1c2cfb23795f91263
Epicnicity322 <christianorangel322@gmail.com>
2020-06-21 02:04:02 -0300

    Start check sub command

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/command/subcommand/CheckSubCommand.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/command/subcommand/CheckSubCommand.java
@@ -1,5 +1,27 @@
 package com.epicnicity322.playmoresounds.bukkit.command.subcommand;
 
-public class CheckSubCommand
+import com.epicnicity322.epicpluginlib.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class CheckSubCommand extends Command
 {
+    @Override
+    public @NotNull String getName()
+    {
+        return "check";
+    }
+
+    @Override
+    public @Nullable String[] getAliases()
+    {
+        return new String[]{"sounds"};
+    }
+
+    @Override
+    public void run(@NotNull String label, @NotNull CommandSender sender, @NotNull String[] args)
+    {
+
+    }
 }
