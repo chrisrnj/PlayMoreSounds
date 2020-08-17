d625da342eb43cd23974f790b08ee9f9f2db6be3
Epicnicity322 <christianorangel322@gmail.com>
2020-08-16 22:00:43 -0300

    Make Hook Plugins addons start correctly

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/PlayMoreSounds.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/PlayMoreSounds.java
@@ -2,6 +2,8 @@ package com.epicnicity322.playmoresounds.bukkit;
 
 import com.epicnicity322.epicpluginlib.bukkit.lang.MessageSender;
 import com.epicnicity322.epicpluginlib.bukkit.logger.Logger;
+import com.epicnicity322.epicpluginlib.bukkit.reflection.ReflectionUtil;
+import com.epicnicity322.epicpluginlib.core.EpicPluginLib;
 import com.epicnicity322.epicpluginlib.core.logger.ConsoleLogger;
 import com.epicnicity322.epicpluginlib.core.logger.ErrorLogger;
 import com.epicnicity322.epicpluginlib.core.tools.Version;
@@ -11,11 +13,16 @@ import com.epicnicity322.playmoresounds.bukkit.sound.SoundManager;
 import com.epicnicity322.playmoresounds.bukkit.util.ListenerRegister;
 import com.epicnicity322.playmoresounds.bukkit.util.UpdateManager;
 import com.epicnicity322.playmoresounds.bukkit.util.VersionUtils;
+import com.epicnicity322.playmoresounds.core.addons.AddonDescription;
 import com.epicnicity322.playmoresounds.core.addons.AddonManager;
+import com.epicnicity322.playmoresounds.core.addons.PMSAddon;
 import com.epicnicity322.playmoresounds.core.addons.StartTime;
 import com.epicnicity322.playmoresounds.core.config.Configurations;
 import com.epicnicity322.playmoresounds.core.util.LoadableHashSet;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -28,8 +35,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.logging.Level;
+import java.util.Map;
 
 public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity322.playmoresounds.core.PlayMoreSounds
 {
@@ -39,11 +47,13 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
     private static final @NotNull LoadableHashSet<String> serverPlugins = new LoadableHashSet<>();
     private static final @NotNull PluginManager pm = Bukkit.getPluginManager();
     private static final @NotNull Path folder = Paths.get("plugins").resolve("PlayMoreSounds");
-    private static final @NotNull Logger logger = new Logger("&6[&9PlayMoreSounds&6] ");
     private static final @NotNull MessageSender messageSender = new MessageSender(Configurations.CONFIG.getPluginConfig(),
             Configurations.LANGUAGE_EN_US.getPluginConfig());
+    private static final @NotNull HashSet<String> enabledPlugins = new HashSet<>();
+    private static @NotNull Logger logger = new Logger("&6[&9PlayMoreSounds&6] ");
     private static @Nullable PlayMoreSounds instance;
-    private static ErrorLogger errorLogger;
+    private static @NotNull ErrorLogger errorLogger;
+    private static boolean success = true;
 
     static {
         // Creating data folder.
@@ -55,25 +65,60 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
             }
         }
 
-        messageSender.addLanguage("EN_US", Configurations.LANGUAGE_EN_US.getPluginConfig());
-        messageSender.addLanguage("ES_LA", Configurations.LANGUAGE_ES_LA.getPluginConfig());
-        messageSender.addLanguage("PT_BR", Configurations.LANGUAGE_PT_BR.getPluginConfig());
-        messageSender.addLanguage("ZH_CN", Configurations.LANGUAGE_ZH_CN.getPluginConfig());
-
         errorLogger = new ErrorLogger(folder, "PlayMoreSounds", getVersion().getVersion(),
-                Collections.singleton("Epicnicity322"), "https://www.spigotmc.org/resources/37429/", null);
+                Collections.singleton("Epicnicity322"), "https://www.spigotmc.org/resources/37429/", Bukkit.getLogger());
+
+        // Checking if EpicPluginLib is on 1.6.1 by checking if version field exists because this field was added on 1.6.1.
+        if (ReflectionUtil.getField(EpicPluginLib.class, "version") == null) {
+            success = false;
+
+            // There were no logger level on version 1.6, so I'm just using bukkit logger.
+            addOnEnableRunnable(() -> Bukkit.getLogger().severe("You are running an old version of EpicPluginLib, make sure you are using the latest one."));
+        } else {
+            messageSender.addLanguage("EN_US", Configurations.LANGUAGE_EN_US.getPluginConfig());
+            messageSender.addLanguage("ES_LA", Configurations.LANGUAGE_ES_LA.getPluginConfig());
+            messageSender.addLanguage("PT_BR", Configurations.LANGUAGE_PT_BR.getPluginConfig());
+            messageSender.addLanguage("ZH_CN", Configurations.LANGUAGE_ZH_CN.getPluginConfig());
+        }
     }
 
     private final @NotNull Path jar = getFile().toPath();
     private final @NotNull AddonManager addonManager;
+    private final @NotNull HashMap<PMSAddon, HashSet<String>> hookingPluginAddons = new HashMap<>();
+    private final @NotNull Listener addonHookingPluginStarter = new Listener()
+    {
+        @EventHandler
+        public void onPluginEnable(PluginEnableEvent event)
+        {
+            enabledPlugins.add(event.getPlugin().getName());
+
+            HashSet<PMSAddon> toStart = new HashSet<>();
+
+            for (Map.Entry<PMSAddon, HashSet<String>> addonHookingPlugin : hookingPluginAddons.entrySet()) {
+                HashSet<String> hookingPlugins = addonHookingPlugin.getValue();
+
+                if (!hookingPlugins.isEmpty() && enabledPlugins.containsAll(hookingPlugins)) {
+                    // Adding addon to toStart set so addons can start all in one thread.
+                    toStart.add(addonHookingPlugin.getKey());
+                }
+            }
+
+            if (!toStart.isEmpty())
+                new Thread(() -> {
+                    for (PMSAddon addon : toStart)
+                        addonManager.startAddon(addon);
+                }, "PMSAddon Runner").start();
+        }
+    };
 
     public PlayMoreSounds()
     {
         instance = this;
+        logger = new Logger(logger.getPrefix(), getLogger());
 
         PluginDescriptionFile descriptionFile = getDescription();
 
-        errorLogger = new ErrorLogger(folder, descriptionFile.getName(), version.toString(), descriptionFile.getAuthors(),
+        errorLogger = new ErrorLogger(folder, descriptionFile.getName(), getVersion().getVersion(), descriptionFile.getAuthors(),
                 descriptionFile.getWebsite(), getLogger());
         addonManager = new AddonManager(this, serverPlugins);
 
@@ -82,7 +127,8 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
                 try {
                     runnable.run();
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    logger.log("&cAn unknown error occurred on PlayMoreSounds initialization.");
+                    errorLogger.report(e, "PMSInitializationError (Unknown):");
                 }
         }).start();
     }
@@ -175,7 +221,7 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
     }
 
     @Override
-    public @NotNull ConsoleLogger<?, ?> getCoreLogger()
+    public @NotNull ConsoleLogger<?> getCoreLogger()
     {
         return logger;
     }
@@ -189,12 +235,12 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
     @Override
     public void onEnable()
     {
-        boolean success = true;
-
         try {
-            if (serverPlugins.isEmpty())
-                for (Plugin plugin : Bukkit.getPluginManager().getPlugins())
-                    serverPlugins.add(plugin.getName());
+            if (!success)
+                return;
+
+            for (Plugin plugin : Bukkit.getPluginManager().getPlugins())
+                serverPlugins.add(plugin.getName());
 
             serverPlugins.setLoaded(true);
 
@@ -206,6 +252,26 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
                 logger.log("&cFailed to register addons.");
                 errorLogger.report(ex, "Addon registration error:");
             }
+
+            for (Plugin plugin : pm.getPlugins())
+                if (plugin.isEnabled())
+                    enabledPlugins.add(plugin.getName());
+
+            for (PMSAddon addon : addonManager.getAddons()) {
+                AddonDescription description = addon.getDescription();
+
+                if (description.getStartTime() == StartTime.HOOK_PLUGINS) {
+                    HashSet<String> hookPlugins = new HashSet<>();
+
+                    for (String plugin : description.getHookPlugins())
+                        if (!enabledPlugins.contains(plugin) && serverPlugins.contains(plugin))
+                            hookPlugins.add(plugin);
+
+                    hookingPluginAddons.put(addon, hookPlugins);
+                }
+            }
+
+            pm.registerEvents(addonHookingPluginStarter, this);
 
             addonManager.startAddons(StartTime.BEFORE_CONFIGURATION);
 
@@ -225,7 +291,7 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
             // Registering region leave event caller.
             pm.registerEvents(new OnPlayerQuit(), this);
             // Registering region enter and leave event caller.
-            pm.registerEvents(new OnPlayerTeleport(), this);
+            pm.registerEvents(new OnPlayerTeleport(this), this);
             //TODO: Design better way to register OnRegionEnterLeave listener
             pm.registerEvents(new OnRegionEnterLeave(this), this);
             // TimeTrigger checks itself it does need to load or not on load method.
@@ -245,47 +311,59 @@ public final class PlayMoreSounds extends JavaPlugin implements com.epicnicity32
             success = false;
             errorLogger.report(e, "PMSLoadingError (Unknown):");
         } finally {
-            logger.log("&6============================================");
-
             if (success) {
+                logger.log("&6============================================");
                 logger.log("&aPlayMoreSounds has been enabled");
                 logger.log("&a" + SoundManager.getSoundTypes().size() + " sounds available on " + VersionUtils.getBukkitVersion());
                 logger.log("&6============================================");
+                //logger.log("&ePlayMoreSounds is collecting anonymous data using bStats. If you don't want to send data, edit bStats configuration.");
+
                 addonManager.startAddons(StartTime.END);
 
-                Bukkit.getScheduler().runTaskLater(this, () -> addonManager.startAddons(StartTime.SERVER_LOAD_COMPLETE), 1);
+                Bukkit.getScheduler().runTaskLater(this, () -> {
+                    // Starting addons that hook to plugins that are not present in the server.
+                    if (!hookingPluginAddons.isEmpty())
+                        new Thread(() -> {
+                            for (Map.Entry<PMSAddon, HashSet<String>> addonHookingPlugin : hookingPluginAddons.entrySet())
+                                if (addonHookingPlugin.getValue().isEmpty())
+                                    addonManager.startAddon(addonHookingPlugin.getKey());
+                        }, "PMSAddon Runner").start();
 
-                new Thread(() -> {
-                    for (Runnable runnable : onEnableRunnables)
-                        try {
-                            runnable.run();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                }).start();
+                    addonManager.startAddons(StartTime.SERVER_LOAD_COMPLETE);
+                }, 1);
             } else {
+                logger.log("&6============================================");
                 logger.log("&cSomething went wrong while loading PMS");
                 logger.log("&cMake sure you read messages before reporting");
                 logger.log("&6============================================");
-                logger.log("&4ERROR.LOG generated, please check.");
-                logger.log("Plugin disabled.", Level.SEVERE);
+                getLogger().severe("Plugin disabled.");
                 Bukkit.getPluginManager().disablePlugin(this);
             }
+
+            new Thread(() -> {
+                for (Runnable runnable : onEnableRunnables)
+                    try {
+                        runnable.run();
+                    } catch (Exception e) {
+                        logger.log("&cAn unknown error occurred on PlayMoreSounds startup.");
+                        errorLogger.report(e, "PMSLoadingError (Unknown):");
+                    }
+            }).start();
         }
     }
 
     @Override
     public void onDisable()
     {
-        if (addonManager != null)
-            addonManager.stopAddons();
+        addonManager.stopAddons();
 
         new Thread(() -> {
             for (Runnable runnable : onDisableRunnables)
                 try {
                     runnable.run();
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    logger.log("&cAn unknown error occurred on PlayMoreSounds shutdown.");
+                    errorLogger.report(e, "PMSUnloadingError (Unknown):");
                 }
         }).start();
     }
