c97f22307b5a650a9cbea45409fe753a5e6e51fb
Epicnicity322 <christianorangel322@gmail.com>
2020-08-16 21:58:52 -0300

    Fix teleport sounds not playing when teleporting long distances

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/listener/OnPlayerTeleport.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/listener/OnPlayerTeleport.java
@@ -1,16 +1,29 @@
 package com.epicnicity322.playmoresounds.bukkit.listener;
 
+import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
 import com.epicnicity322.playmoresounds.bukkit.sound.RichSound;
 import com.epicnicity322.playmoresounds.core.config.Configurations;
 import com.epicnicity322.yamlhandler.ConfigurationSection;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.jetbrains.annotations.NotNull;
 
 public final class OnPlayerTeleport implements Listener
 {
+    private final @NotNull PlayMoreSounds main;
+    private final @NotNull BukkitScheduler scheduler;
+
+    public OnPlayerTeleport(@NotNull PlayMoreSounds main)
+    {
+        this.main = main;
+        scheduler = Bukkit.getScheduler();
+    }
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerTeleport(PlayerTeleportEvent event)
     {
@@ -26,8 +39,8 @@ public final class OnPlayerTeleport implements Listener
             if (section != null) {
                 RichSound sound = new RichSound(section);
 
-                if (!event.isCancelled() || !sound.isCancellable())
-                    sound.play(event.getPlayer());
+                if (sound.isEnabled() && (!event.isCancelled() || !sound.isCancellable()))
+                    scheduler.runTaskLater(main, () -> sound.play(event.getPlayer()), 1);
             }
         }
     }
