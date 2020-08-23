b0842c15036f97cedb8a9b50300ec381de4b2c04
Epicnicity322 <christianorangel322@gmail.com>
2020-08-23 20:12:49 -0300

    Implement default methods and update documentation.

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/Playable.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/Playable.java
@@ -8,30 +8,37 @@ import org.jetbrains.annotations.Nullable;
 public interface Playable
 {
     /**
-     * Plays a sound in a specific location. If your {@link SoundOptions#getRadius()} is 0, then the sound is not played
-     * at all. Also, this does not handle {@link Player#canSee(Player)} and Invisibility effects since there is no player
-     * to check.
+     * Plays a sound in a specific location. If your {@link SoundOptions#getRadius()} is 0, then the sound is not played.
      *
-     * @param sourceLocation The location where the sound will take into account. May change depending
-     *                       on {@link SoundOptions#getRelativeLocation()} and {@link SoundOptions#isEyeLocation()}.
+     * @param sourceLocation The location where the sound will play. May change depending on
+     *                       {@link SoundOptions#getRelativeLocation()} and {@link SoundOptions#isEyeLocation()}.
+     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
-    void play(@NotNull Location sourceLocation);
+    default void play(@NotNull Location sourceLocation)
+    {
+        play(null, sourceLocation);
+    }
 
     /**
      * Plays a sound to a specific player. Depending on your {@link SoundOptions#getRadius()}, the sound may be played to
      * other players too.
      *
-     * @param player The player who triggered the sound.
+     * @param player The player to play the sound.
+     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
-    void play(@NotNull Player player);
+    default void play(@NotNull Player player)
+    {
+        play(player, player.getLocation());
+    }
 
     /**
-     * Plays a sound to a specific player in a specific location. Depending on your {@link SoundOptions#getRadius()},
-     * the sound may be played to other players too at different locations.
+     * Plays a sound to a specific player in a specific location. Depending on {@link SoundOptions#getRadius()}, the
+     * sound may play to other players at different locations.
      *
-     * @param player         The player who triggered the sound.
-     * @param sourceLocation The location where the sound will take into account. May change depending
-     *                       on {@link SoundOptions#getRelativeLocation()} and {@link SoundOptions#isEyeLocation()}.
+     * @param player         The player to play the sound.
+     * @param sourceLocation The location where the sound will play. May change depending on
+     *                       {@link SoundOptions#getRelativeLocation()} and {@link SoundOptions#isEyeLocation()}.
+     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
     void play(@Nullable Player player, @NotNull Location sourceLocation);
 }
