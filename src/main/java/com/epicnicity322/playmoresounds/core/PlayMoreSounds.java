086e21906d21fc433ccc6c465b7594f6a0ca8a24
Epicnicity322 <christianorangel322@gmail.com>
2020-11-10 23:04:01 -0300

    Change version to 3.3.0

--- src/main/java/com/epicnicity322/playmoresounds/core/PlayMoreSounds.java
+++ src/main/java/com/epicnicity322/playmoresounds/core/PlayMoreSounds.java
@@ -29,7 +29,7 @@ import java.nio.file.Path;
 
 public interface PlayMoreSounds
 {
-    @NotNull String versionString = "3.2.0";
+    @NotNull String versionString = "3.3.0";
 
     @NotNull Version version = new Version(versionString);
 
