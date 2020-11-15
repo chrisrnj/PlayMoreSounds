c712c0d6b96b50a149da4d1d4d4c36e983336f41
Epicnicity322 <christianorangel322@gmail.com>
2020-11-15 06:50:50 -0300

    Add 'Plugin Hooks' and 'Addon Hooks' settings, rename 'getMain' to 'getMainClass' and optimize

--- src/main/java/com/epicnicity322/playmoresounds/core/addons/AddonDescription.java
+++ src/main/java/com/epicnicity322/playmoresounds/core/addons/AddonDescription.java
@@ -33,19 +33,26 @@ import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 
 public class AddonDescription
 {
     private static final @NotNull YamlConfigurationLoader loader = new YamlConfigurationLoader();
-    private final @NotNull String main;
+    private static final @NotNull Pattern duplicatedSpaces = Pattern.compile(" +");
+    private static final @NotNull Pattern notLetters = Pattern.compile("[^A-Za-z]");
+    private static final @NotNull Pattern notAlphaNumericAndSpace = Pattern.compile("[^A-Za-z0-9\\s]");
+    private final @NotNull String mainClass;
     private final @NotNull String name;
     private final @NotNull Version version;
     private final @NotNull Version apiVersion;
     private final @NotNull StartTime startTime;
     private final @NotNull Collection<String> authors;
+    private final @NotNull Collection<String> pluginHooks;
+    private final @NotNull Collection<String> addonHooks;
     private final @NotNull Collection<String> requiredPlugins;
     private final @NotNull Collection<String> requiredAddons;
 
@@ -63,8 +70,13 @@ public class AddonDescription
         try {
             Configuration description = loader.load(new InputStreamReader(jarFile.getInputStream(entry)));
 
-            name = parseName(description.getString("Name").orElseThrow(() -> new InvalidAddonException("The addon '" + fileName + "' does not contain 'Name' property in description.")));
-            main = description.getString("Main Class").orElseThrow(() -> new InvalidAddonException("The addon '" + name + "' does not contain 'Main Class' property in description."));
+            try {
+                name = parseName(description.getString("Name").orElseThrow(() -> new InvalidAddonException("The addon '" + fileName + "' does not contain 'Name' property in description.")));
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidAddonException("The addon '" + fileName + "' has an invalid name: " + ex.getMessage(), ex);
+            }
+
+            mainClass = description.getString("Main Class").orElseThrow(() -> new InvalidAddonException("The addon '" + name + "' does not contain 'Main Class' property in description."));
 
             Optional<String> startTime = description.getString("Start Time");
 
@@ -77,9 +89,21 @@ public class AddonDescription
             else
                 this.startTime = StartTime.SERVER_LOAD_COMPLETE;
 
-            version = new Version(description.getString("Version").orElse("1.0"));
-            apiVersion = new Version(description.getString("Api Version").orElse(PlayMoreSounds.version.getVersion()));
+            try {
+                version = new Version(description.getString("Version").orElse("1.0"));
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidAddonException("The addon '" + name + "' has an invalid version defined for Version key.", ex);
+            }
+
+            try {
+                apiVersion = new Version(description.getString("Api Version").orElse(PlayMoreSounds.version.getVersion()));
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidAddonException("The addon '" + name + "' has an invalid version defined for Api Version key.", ex);
+            }
+
             authors = Collections.unmodifiableCollection(description.getCollection("Authors", Object::toString));
+            pluginHooks = Collections.unmodifiableCollection(description.getCollection("Plugin Hooks", Object::toString));
+            addonHooks = Collections.unmodifiableCollection(description.getCollection("Addon Hooks", Object::toString));
             requiredPlugins = Collections.unmodifiableCollection(description.getCollection("Required Plugins", Object::toString));
             requiredAddons = Collections.unmodifiableCollection(description.getCollection("Required Addons", Object::toString));
         } catch (InvalidConfigurationException ex) {
@@ -87,28 +111,36 @@ public class AddonDescription
         }
     }
 
+    /**
+     * For better organization addons names must:
+     * - Be not greater than 26 characters.
+     * - Have at least 3 characters.
+     * - Have at least 3 letters.
+     * - Contain only letters, numbers and spaces.
+     */
     private String parseName(String name)
     {
-        name = name.trim().replaceAll(" +", " ");
-
-        if (name.length() < 3)
-            throw new IllegalArgumentException("Addons names length can not be lower than 3.");
-
-        if (name.replaceAll("[^A-Za-z]", "").length() < 3)
-            throw new IllegalArgumentException("Addons names may contain at least 3 letters.");
+        // Trimming and removing duplicated spaces.
+        name = duplicatedSpaces.matcher(name.trim()).replaceAll(" ");
 
         if (name.length() > 26)
-            throw new IllegalArgumentException("Addons names length can not be greater than 26.");
+            throw new IllegalArgumentException("Addon names length can not be greater than 26.");
 
-        if (!name.replaceAll("[A-Za-z0-9\\s]", "").equals(""))
-            throw new IllegalArgumentException("Addons names may only contain letters, numbers and spaces.");
+        if (notLetters.matcher(name).replaceAll("").length() < 3)
+            throw new IllegalArgumentException("Addon names may contain at least 3 letters.");
+
+        if (name.length() < 3)
+            throw new IllegalArgumentException("Addon names length can not be lower than 3.");
+
+        if (notAlphaNumericAndSpace.matcher(name).find())
+            throw new IllegalArgumentException("Addon names may only contain alphanumeric and space characters.");
 
         return name;
     }
 
-    public @NotNull String getMain()
+    public @NotNull String getMainClass()
     {
-        return main;
+        return mainClass;
     }
 
     public @NotNull String getName()
@@ -136,6 +168,16 @@ public class AddonDescription
         return authors;
     }
 
+    public @NotNull Collection<String> getPluginHooks()
+    {
+        return pluginHooks;
+    }
+
+    public @NotNull Collection<String> getAddonHooks()
+    {
+        return addonHooks;
+    }
+
     public @NotNull Collection<String> getRequiredPlugins()
     {
         return requiredPlugins;
@@ -144,5 +186,46 @@ public class AddonDescription
     public @NotNull Collection<String> getRequiredAddons()
     {
         return requiredAddons;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "AddonDescription{" +
+                "mainClass='" + mainClass + '\'' +
+                ", name='" + name + '\'' +
+                ", version=" + version +
+                ", apiVersion=" + apiVersion +
+                ", startTime=" + startTime +
+                ", authors=" + authors +
+                ", pluginHooks=" + pluginHooks +
+                ", addonHooks=" + addonHooks +
+                ", requiredPlugins=" + requiredPlugins +
+                ", requiredAddons=" + requiredAddons +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddonDescription that = (AddonDescription) o;
+        return mainClass.equals(that.mainClass) &&
+                name.equals(that.name) &&
+                version.equals(that.version) &&
+                apiVersion.equals(that.apiVersion) &&
+                startTime == that.startTime &&
+                authors.equals(that.authors) &&
+                pluginHooks.equals(that.pluginHooks) &&
+                addonHooks.equals(that.addonHooks) &&
+                requiredPlugins.equals(that.requiredPlugins) &&
+                requiredAddons.equals(that.requiredAddons);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(mainClass, name, version, apiVersion, startTime, authors, pluginHooks, addonHooks, requiredPlugins, requiredAddons);
     }
 }
