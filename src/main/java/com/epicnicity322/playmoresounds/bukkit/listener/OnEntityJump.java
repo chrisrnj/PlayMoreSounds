a91241d753360906e3847f2ea631ee309f5f41d9
Epicnicity322 <christianorangel322@gmail.com>
2020-11-16 05:23:49 -0300

    Add Entity Jump and Player Jump events

---
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/listener/OnEntityJump.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Christiano Rangel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.epicnicity322.playmoresounds.bukkit.listener;
+
+import com.destroystokyo.paper.event.entity.EntityJumpEvent;
+import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
+import com.epicnicity322.playmoresounds.bukkit.sound.RichSound;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.jetbrains.annotations.NotNull;
+
+public class OnEntityJump extends PMSListener
+{
+    public OnEntityJump(@NotNull PlayMoreSounds plugin)
+    {
+        super(plugin);
+    }
+
+    @Override
+    public @NotNull String getName()
+    {
+        return "Entity Jump";
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityJump(EntityJumpEvent event)
+    {
+        RichSound sound = getRichSound();
+
+        if (!event.isCancelled() || !sound.isCancellable())
+            sound.play(event.getEntity().getLocation());
+    }
+}
