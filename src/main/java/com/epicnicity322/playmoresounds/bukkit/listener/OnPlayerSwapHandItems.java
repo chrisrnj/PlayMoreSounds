07cd525ecefb3ce4452ff8f1c7c98d48755241c5
Epicnicity322 <christianorangel322@gmail.com>
2021-05-31 20:59:45 -0300

    Add Swap Hand Item event sound

---
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/listener/OnPlayerSwapHandItems.java
@@ -0,0 +1,49 @@
+/*
+ * PlayMoreSounds - A bukkit plugin that manages and plays sounds.
+ * Copyright (C) 2021 Christiano Rangel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.epicnicity322.playmoresounds.bukkit.listener;
+
+import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
+import com.epicnicity322.playmoresounds.bukkit.sound.RichSound;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class OnPlayerSwapHandItems extends PMSListener
+{
+    public OnPlayerSwapHandItems(@NotNull PlayMoreSounds plugin)
+    {
+        super(plugin);
+    }
+
+    @Override
+    public @NotNull String getName()
+    {
+        return "Swap Hand Item";
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event)
+    {
+        RichSound sound = getRichSound();
+
+        if (!event.isCancelled() || !sound.isCancellable())
+            sound.play(event.getPlayer());
+    }
+}
