67d9f3c991aaf9a4ce0edf3363c9d3d0ef2e58e4
Epicnicity322 <christianorangel322@gmail.com>
2021-05-31 21:05:00 -0300

    Update documentation

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/Playable.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/Playable.java
@@ -18,6 +18,7 @@
 
 package com.epicnicity322.playmoresounds.bukkit.sound;
 
+import com.epicnicity322.playmoresounds.core.sound.SoundOptions;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -29,7 +30,6 @@ public interface Playable
      * Plays a sound in a specific location. If your {@link SoundOptions#getRadius()} is 0, then the sound is not played.
      *
      * @param sourceLocation The location where the sound will play. May change depending on {@link SoundOptions#getRelativeLocation()}.
-     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
     default void play(@NotNull Location sourceLocation)
     {
@@ -41,7 +41,6 @@ public interface Playable
      * other players too.
      *
      * @param player The player to play the sound.
-     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
     default void play(@NotNull Player player)
     {
@@ -54,7 +53,6 @@ public interface Playable
      *
      * @param player         The player to play the sound.
      * @param sourceLocation The location where the sound will play. May change depending on {@link SoundOptions#getRelativeLocation()}.
-     * @throws IllegalStateException If the sound has a delay and PlayMoreSounds was not instantiated by bukkit yet.
      */
     void play(@Nullable Player player, @NotNull Location sourceLocation);
 }
