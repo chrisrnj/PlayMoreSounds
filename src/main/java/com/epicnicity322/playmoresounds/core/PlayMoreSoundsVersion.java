e5675002bdfd031d1c02fbfbaf3977128b0cf292
Epicnicity322 <christianorangel322@gmail.com>
2021-07-16 01:01:19 -0300

    Change version to 4.1

--- src/main/java/com/epicnicity322/playmoresounds/core/PlayMoreSoundsVersion.java
+++ src/main/java/com/epicnicity322/playmoresounds/core/PlayMoreSoundsVersion.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.NotNull;
 
 public final class PlayMoreSoundsVersion
 {
-    public static final @NotNull String version = "4.0.4";
+    public static final @NotNull String version = "4.1";
     private static final @NotNull Version versionVersion = new Version(version);
 
     /**
