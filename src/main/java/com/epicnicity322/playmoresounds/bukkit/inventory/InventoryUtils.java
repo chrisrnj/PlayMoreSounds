6eea64bc5087999719b37acc89424ed37d577a0e
Epicnicity322 <christianorangel322@gmail.com>
2022-01-15 15:08:13 -0300

    Generalize GUI creation methods

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/inventory/InventoryUtils.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/inventory/InventoryUtils.java
@@ -1,6 +1,6 @@
 /*
  * PlayMoreSounds - A bukkit plugin that manages and plays sounds.
- * Copyright (C) 2021 Christiano Rangel
+ * Copyright (C) 2022 Christiano Rangel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,17 +19,33 @@
 package com.epicnicity322.playmoresounds.bukkit.inventory;
 
 import com.epicnicity322.epicpluginlib.core.tools.Version;
+import com.epicnicity322.epicpluginlib.core.util.ObjectUtils;
+import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
 import com.epicnicity322.playmoresounds.bukkit.util.VersionUtils;
 import com.epicnicity322.playmoresounds.core.PlayMoreSoundsCore;
+import com.epicnicity322.playmoresounds.core.config.Configurations;
+import com.epicnicity322.yamlhandler.Configuration;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.HashMap;
 
+@SuppressWarnings("deprecation")
 public final class InventoryUtils
 {
     private static final @NotNull Material glassPanel;
@@ -52,13 +68,10 @@ public final class InventoryUtils
      * @param inventory  The inventory to fill.
      * @param from_index The slot to start the filling.
      * @param to_index   The slot to stop the filling.
-     * @return A set with all the glass panes that filled the inventory.
      */
-    public static @NotNull HashSet<ItemStack> fillWithGlass(@NotNull Inventory inventory, int from_index, int to_index)
+    public static void fillWithGlass(@NotNull Inventory inventory, int from_index, int to_index)
     {
-        HashSet<ItemStack> glassPanes = new HashSet<>();
-
-        if (from_index < 0 || to_index < 0 || from_index > 53 || to_index > 53) return glassPanes;
+        if (from_index < 0 || to_index < 0 || from_index > 53 || to_index > 53) return;
 
         for (int slot = from_index; slot <= to_index; ++slot) {
             if (inventory.getItem(slot) != null) continue;
@@ -70,9 +83,118 @@ public final class InventoryUtils
             if (VersionUtils.hasItemFlags()) meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
             glassPane.setItemMeta(meta);
             inventory.setItem(slot, glassPane);
-            glassPanes.add(glassPane);
+        }
+    }
+
+    public static @NotNull ItemStack getItemStack(@NotNull String inventory, @NotNull String name)
+    {
+        Configuration config = Configurations.CONFIG.getConfigurationHolder().getConfiguration();
+        ItemStack itemStack = new ItemStack(ObjectUtils.getOrDefault(Material.matchMaterial(config.getString(inventory + " Inventory." + name + " Item.Material").orElse("STONE")), Material.STONE));
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName(PlayMoreSounds.getLanguage().getColored(inventory + ".Inventory." + name + ".Display Name"));
+        itemMeta.setLore(Arrays.asList(PlayMoreSounds.getLanguage().getColored(inventory + ".Inventory." + name + ".Lore").split("<line>")));
+
+        if (config.getBoolean(inventory + " Inventory." + name + " Item.Glowing").orElse(false))
+            itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+        if (VersionUtils.hasItemFlags())
+            itemMeta.addItemFlags(ItemFlag.values());
+
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    private static final @NotNull HashMap<HumanEntity, HashMap<Integer, Runnable>> openInventories = new HashMap<>();
+    private static final @NotNull HashMap<HumanEntity, Runnable> onClose = new HashMap<>();
+
+    private static final @NotNull Listener inventoryListener = new Listener()
+    {
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onInventoryClick(InventoryClickEvent event)
+        {
+            if (event.getClickedInventory() == null) return;
+
+            HumanEntity player = event.getWhoClicked();
+            HashMap<Integer, Runnable> buttons = openInventories.get(player);
+
+            if (buttons == null) return;
+
+            event.setCancelled(true);
+
+            Runnable button = buttons.get(event.getRawSlot());
+
+            if (button != null) try {
+                button.run();
+            } catch (Throwable t) {
+                PlayMoreSoundsCore.getErrorHandler().report(t, "Button Click Error:");
+            }
         }
 
-        return glassPanes;
+        @EventHandler
+        public void onInventoryClose(InventoryCloseEvent event)
+        {
+            HumanEntity player = event.getPlayer();
+
+            if (openInventories.remove(player) != null && openInventories.isEmpty()) {
+                HandlerList.unregisterAll(this);
+
+                Runnable runnable = onClose.remove(player);
+
+                if (runnable != null) try {
+                    runnable.run();
+                } catch (Throwable t) {
+                    PlayMoreSoundsCore.getErrorHandler().report(t, "On Close Error:");
+                }
+            }
+        }
+    };
+
+    /**
+     * Opens an inventory that can't have its items moved/stolen.
+     *
+     * @param inventory The inventory to open.
+     * @param player    The player to open the inventory to.
+     * @see #openInventory(Inventory inventory, HashMap buttons, HumanEntity player, Runnable onClose)
+     */
+    public static void openInventory(@NotNull Inventory inventory, @NotNull HumanEntity player)
+    {
+        openInventory(inventory, null, player, null);
+    }
+
+    /**
+     * Opens an inventory that runs a {@link Runnable} when the viewer clicks the specified slot.
+     * The items in this inventory can not be moved/stolen.
+     *
+     * @param inventory The inventory to open.
+     * @param buttons   The map with the number of the slot that when clicked will run the {@link Runnable}.
+     * @param player    The player to open the inventory to.
+     * @see #openInventory(Inventory inventory, HashMap buttons, HumanEntity player, Runnable onClose)
+     */
+    public static void openInventory(@NotNull Inventory inventory, @NotNull HashMap<Integer, Runnable> buttons, @NotNull HumanEntity player)
+    {
+        openInventory(inventory, buttons, player, null);
+    }
+
+    /**
+     * Opens an inventory that runs a {@link Runnable} when the viewer clicks the specified slot and another {@link Runnable} when the inventory closes.
+     * The items in this inventory can not be moved/stolen.
+     *
+     * @param inventory The inventory to open.
+     * @param buttons The map with the number of the slot that when clicked will run the {@link Runnable}.
+     * @param player The player to open the inventory to.
+     * @param onClose The runnable to run when the inventory is closed.
+     */
+    public static void openInventory(@NotNull Inventory inventory, @Nullable HashMap<Integer, Runnable> buttons, @NotNull HumanEntity player, @Nullable Runnable onClose)
+    {
+        if (PlayMoreSounds.getInstance() == null) throw new IllegalStateException("PlayMoreSounds is not loaded.");
+
+        player.openInventory(inventory);
+
+        if (openInventories.isEmpty())
+            Bukkit.getPluginManager().registerEvents(inventoryListener, PlayMoreSounds.getInstance());
+
+        if (buttons == null) buttons = new HashMap<>(0);
+
+        openInventories.put(player, buttons);
     }
 }
