a65b4af65d18fc088613f7aa4ec4184b70f7b9b6
Epicnicity322 <christianorangel322@gmail.com>
2022-01-23 18:55:37 -0300

    Add 'list' argument to addons command

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/command/subcommand/AddonsSubCommand.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/command/subcommand/AddonsSubCommand.java
@@ -23,19 +23,26 @@ import com.epicnicity322.epicpluginlib.bukkit.command.CommandRunnable;
 import com.epicnicity322.epicpluginlib.bukkit.lang.MessageSender;
 import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
 import com.epicnicity322.playmoresounds.bukkit.inventory.AddonsInventory;
+import com.epicnicity322.playmoresounds.core.addons.PMSAddon;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashSet;
+
 public final class AddonsSubCommand extends Command implements Helpable
 {
-    private static final @NotNull MessageSender lang = PlayMoreSounds.getLanguage();
-
     @Override
     public @NotNull String getName()
     {
         return "addons";
+    }
+
+    @Override
+    public @Nullable String[] getAliases()
+    {
+        return new String[]{"addon"};
     }
 
     @Override
@@ -47,25 +54,58 @@ public final class AddonsSubCommand extends Command implements Helpable
     @Override
     protected @Nullable CommandRunnable getNoPermissionRunnable()
     {
-        return (label, sender, args) -> lang.send(sender, lang.get("General.No Permission"));
+        return (label, sender, args) -> PlayMoreSounds.getLanguage().send(sender, PlayMoreSounds.getLanguage().get("General.No Permission"));
     }
 
     @Override
     public @NotNull CommandRunnable onHelp()
     {
-        return (label, sender, args) -> lang.send(sender, false, lang.get("Help.Addons").replace("<label>", label));
+        return (label, sender, args) -> PlayMoreSounds.getLanguage().send(sender, false, PlayMoreSounds.getLanguage().get("Help.Addons").replace("<label>", label));
     }
 
     @Override
     public void run(@NotNull String label, @NotNull CommandSender sender, @NotNull String[] args)
     {
+        MessageSender lang = PlayMoreSounds.getLanguage();
+
+        if (args.length > 1) {
+            if (args[1].equalsIgnoreCase("list")) {
+                if (!sender.hasPermission("playmoresounds.addons.list")) {
+                    lang.send(sender, lang.get("General.No Permission"));
+                    return;
+                }
+
+                lang.send(sender, lang.get("Addons.List.Header"));
+
+                HashSet<PMSAddon> addons = PlayMoreSounds.getAddonManager().getAddons();
+                StringBuilder data = new StringBuilder();
+                int count = 0;
+
+                for (PMSAddon addon : addons) {
+                    data.append(addon.isLoaded() ? "&a" : "&c").append(addon.getDescription().getName());
+
+                    if (++count != addons.size()) {
+                        data.append(lang.get("Addons.List.Separator", "&f, "));
+                    }
+                }
+
+                lang.send(sender, false, data.toString());
+            } else {
+                lang.send(sender, lang.get("General.Invalid Arguments").replace("<label>", label).replace("<label2>", args[0]).replace("<args>", "<list|start|stop>"));
+            }
+
+            return;
+        }
+
         if (!(sender instanceof HumanEntity)) {
             lang.send(sender, lang.get("General.Not A Player"));
             return;
         }
+        if (!sender.hasPermission("playmoresounds.addons.management")) {
+            lang.send(sender, lang.get("General.No Permission"));
+            return;
+        }
 
-        AddonsInventory inventory = new AddonsInventory();
-
-        inventory.openInventory((HumanEntity) sender);
+        new AddonsInventory().openInventory((HumanEntity) sender);
     }
 }
