2f07b23ea5e8b99866c4f6049c713a769d855be2
Epicnicity322 <christianorangel322@gmail.com>
2022-03-01 21:42:29 -0300

    Remove Relative Location and World Black List

--- src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/PlayableSound.java
+++ src/main/java/com/epicnicity322/playmoresounds/bukkit/sound/PlayableSound.java
@@ -18,12 +18,8 @@
 
 package com.epicnicity322.playmoresounds.bukkit.sound;
 
-import com.epicnicity322.epicpluginlib.bukkit.reflection.ReflectionUtil;
-import com.epicnicity322.epicpluginlib.bukkit.reflection.type.PackageType;
 import com.epicnicity322.playmoresounds.bukkit.PlayMoreSounds;
 import com.epicnicity322.playmoresounds.bukkit.sound.events.PlaySoundEvent;
-import com.epicnicity322.playmoresounds.bukkit.sound.events.PrePlaySoundEvent;
-import com.epicnicity322.playmoresounds.core.config.Configurations;
 import com.epicnicity322.playmoresounds.core.sound.Sound;
 import com.epicnicity322.playmoresounds.core.sound.SoundCategory;
 import com.epicnicity322.playmoresounds.core.sound.SoundOptions;
@@ -36,87 +32,16 @@ import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Constructor;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
 
 public class PlayableSound extends Sound implements Playable
 {
-    private static final @NotNull HashSet<String> blackListedWorlds = new HashSet<>();
-    private static final @NotNull Validator validator;
-    private static final @NotNull SoundPlayer soundPlayer;
-
-    static {
-        // PlayMoreSounds might be used on versions where GameMode.SPECTATOR is not a thing.
-        Validator noSpectator = (player, permissionRequired) -> (permissionRequired != null && !player.hasPermission(permissionRequired)) || (player.hasPotionEffect(PotionEffectType.INVISIBILITY) && player.hasPermission("playmoresounds.bypass.invisibility"));
-
-        if (GameMode.values().length >= 4) {
-            validator = (player, permissionRequired) -> noSpectator.isInvalidPlayer(player, permissionRequired) || player.getGameMode() == GameMode.SPECTATOR;
-        } else {
-            validator = noSpectator;
-        }
-
-        // Checking if SoundCategory is present.
-        boolean hasSoundCategory = ReflectionUtil.getClass("org.bukkit.SoundCategory") != null;
-        // Checking if custom sounds are present.
-        boolean hasCustomSounds = false;
-
-        try {
-            Player.class.getMethod("playSound", Location.class, String.class, float.class, float.class);
-            hasCustomSounds = true;
-        } catch (NoSuchMethodException ignored) {
-        }
-
-        // Setting the sound player
-        if (hasSoundCategory) {
-            soundPlayer = (player, location, sound) -> player.playSound(location, sound.getSound(), sound.getCategory().asBukkit(), sound.getVolume(), sound.getPitch());
-        } else {
-            if (hasCustomSounds) {
-                soundPlayer = (player, location, sound) -> player.playSound(location, sound.getSound(), sound.getVolume(), sound.getPitch());
-            } else {
-                // Versions before Player#playSound(Location,String,float,float) used to have Packet62NamedSoundEffect, sending packet manually.
-                Class<?> packet62NamedSoundEffect_class = ReflectionUtil.getClass("Packet62NamedSoundEffect", PackageType.MINECRAFT_SERVER);
-                Constructor<?> packet62NamedSoundEffect_constructor = null;
-
-                try {
-                    if (packet62NamedSoundEffect_class != null) {
-                        packet62NamedSoundEffect_constructor = packet62NamedSoundEffect_class.getConstructor(String.class, double.class, double.class, double.class, float.class, float.class);
-                    }
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                }
-
-                Constructor<?> finalPacket62NamedSoundEffect_constructor = packet62NamedSoundEffect_constructor;
-
-                soundPlayer = (player, location, sound) -> {
-                    try {
-                        ReflectionUtil.sendPacket(player, finalPacket62NamedSoundEffect_constructor.newInstance(sound.getSound(), location.getX() + 0.5, location.getY() + 0.5, location.getZ() + 0.5, sound.getVolume(), sound.getPitch()));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                };
-            }
-        }
-
-        // Updating blackListedWorlds, this way it doesn't need to read from config when playing a sound.
-        Runnable blackListedWorldsUpdater = () -> {
-            synchronized (blackListedWorlds) {
-                blackListedWorlds.clear();
-                blackListedWorlds.addAll(Configurations.CONFIG.getConfigurationHolder().getConfiguration().getCollection("World Black List", Object::toString));
-            }
-        };
-
-        blackListedWorldsUpdater.run();
-        PlayMoreSounds.onReload(blackListedWorldsUpdater);
-    }
-
-    private PlayMoreSounds plugin = PlayMoreSounds.getInstance();
-
     public PlayableSound(@NotNull String sound, @Nullable SoundCategory category, float volume, float pitch, long delay, @Nullable SoundOptions options)
     {
         super(sound, category, volume, pitch, delay, options);
 
-        if (delay > 0 && plugin == null) {
+        if (delay > 0 && PlayMoreSounds.getInstance() == null) {
             throw new UnsupportedOperationException("PlayMoreSounds must be enabled to play delayed sounds.");
         }
     }
@@ -125,7 +50,7 @@ public class PlayableSound extends Sound implements Playable
     {
         super(section);
 
-        if (getDelay() > 0 && plugin == null) {
+        if (getDelay() > 0 && PlayMoreSounds.getInstance() == null) {
             throw new UnsupportedOperationException("PlayMoreSounds must be enabled to play delayed sounds.");
         }
     }
@@ -133,9 +58,7 @@ public class PlayableSound extends Sound implements Playable
     @Override
     public void setDelay(long delay)
     {
-        plugin = PlayMoreSounds.getInstance();
-
-        if (delay > 0 && plugin == null) {
+        if (delay > 0 && PlayMoreSounds.getInstance() == null) {
             throw new UnsupportedOperationException("PlayMoreSounds must be enabled to play delayed sounds.");
         }
 
@@ -143,73 +66,57 @@ public class PlayableSound extends Sound implements Playable
     }
 
     @Override
-    public void play(@Nullable Player player, @NotNull Location sourceLocation)
+    public void play(@Nullable Player player, @NotNull Location location)
     {
-        PrePlaySoundEvent preEvent = new PrePlaySoundEvent(player, sourceLocation, this);
+        var options = getOptions();
+        double radius = options.getRadius();
 
-        Bukkit.getPluginManager().callEvent(preEvent);
+        if (player != null) {
+            var permission = options.getPermissionRequired();
 
-        // Validating if the sound should be played for this player.
-        if (preEvent.isCancelled() || (player != null && validator.isInvalidPlayer(player, getOptions().getPermissionRequired())))
-            return;
+            if (permission != null && !player.hasPermission(permission)) {
+                return;
+            }
+            if (player.getGameMode() == GameMode.SPECTATOR || (player.hasPotionEffect(PotionEffectType.INVISIBILITY) && player.hasPermission("playmoresounds.bypass.invisibility"))) {
+                radius = 0;
+            }
+        }
 
-        Location soundLocation = SoundManager.addRelativeLocation(preEvent.getLocation(), getOptions().getRelativeLocation());
-        Collection<Player> players = SoundManager.getInRange(getOptions().getRadiusSquared(), soundLocation);
+        Collection<Player> listeners;
 
-        if (player != null && getOptions().getRadiusSquared() == 0) players.add(player);
+        if (player != null && radius == 0) listeners = Collections.singleton(player);
+        else listeners = SoundManager.getInRange(radius, location);
 
         if (getDelay() == 0) {
-            play(player, players, soundLocation);
+            play(player, listeners, location);
         } else {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> play(player, players, soundLocation), getDelay());
+            final Location finalLocation = location;
+            Bukkit.getScheduler().runTaskLater(PlayMoreSounds.getInstance(), () -> play(player, listeners, finalLocation), getDelay());
         }
     }
 
-    private void play(@Nullable Player sourcePlayer, @NotNull Collection<Player> players, @NotNull Location soundLocation)
+    private void play(@Nullable Player sourcePlayer, @NotNull Collection<Player> listeners, @NotNull Location soundLocation)
     {
-        SoundOptions options = getOptions();
+        var options = getOptions();
 
-        for (Player inRange : players) {
-            if (!blackListedWorlds.contains(inRange.getWorld().getName())
-                    && (options.ignoresDisabled() || SoundManager.getSoundsState(inRange))
-                    && (options.getPermissionToListen() == null || inRange.hasPermission(options.getPermissionToListen()))
-                    && (sourcePlayer == null || inRange.canSee(sourcePlayer))) {
-                Location fixedLocation = soundLocation;
+        for (Player listener : listeners) {
+            // Validating if listener is allowed to hear this sound.
+            if ((options.ignoresDisabled() || SoundManager.getSoundsState(listener))
+                    && (options.getPermissionToListen() == null || listener.hasPermission(options.getPermissionToListen()))
+                    && (sourcePlayer == null || listener.canSee(sourcePlayer))) {
+                Location finalLocation = soundLocation;
 
+                // Radius lower than 0 is global and must be set to the listener's location.
                 if (options.getRadius() < 0)
-                    fixedLocation = SoundManager.addRelativeLocation(inRange.getLocation(), options.getRelativeLocation());
+                    finalLocation = listener.getLocation();
 
-                PlaySoundEvent event = new PlaySoundEvent(this, inRange, fixedLocation, players, sourcePlayer, soundLocation);
+                var event = new PlaySoundEvent(this, listener, finalLocation, listeners, sourcePlayer, soundLocation);
 
                 Bukkit.getPluginManager().callEvent(event);
 
                 if (!event.isCancelled())
-                    soundPlayer.play(inRange, event.getLocation(), this);
+                    listener.playSound(event.getLocation(), getSound(), getCategory().asBukkit(), getVolume(), getPitch());
             }
         }
-    }
-
-    private interface SoundPlayer
-    {
-        /**
-         * Plays the sound to the player.
-         *
-         * @param player   The player to play the sound.
-         * @param location The location the sound should be played at.
-         * @param pmsSound The sound to play.
-         */
-        void play(@NotNull Player player, @NotNull Location location, @NotNull PlayableSound pmsSound);
-    }
-
-    private interface Validator
-    {
-        /**
-         * Validates if the sound should be played to the {@param player}.
-         *
-         * @param player             The player to validate.
-         * @param permissionRequired The permission required to check if the player has it.
-         * @return If the sound should be played.
-         */
-        boolean isInvalidPlayer(@NotNull Player player, @Nullable String permissionRequired);
     }
 }
